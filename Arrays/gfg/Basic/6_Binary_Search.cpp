dc4936efa213f3407c1fdd3e0f96d6133841b241
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-01-01 04:37:44 +0530

    Day 1

---
+++ Arrays/gfg/Basic/6_Binary_Search.cpp
@@ -0,0 +1,31 @@
+// Given a sorted array of size N and an integer K, find the position at which K is present in the array using binary search.
+
+// Example 1:
+
+// Input:
+// N = 5
+// arr[] = {1 2 3 4 5} 
+// K = 4
+// Output: 3
+// Explanation: 4 appears at index 3.
+
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int low = 0;
+        int high = n - 1;
+        int mid;
+        while(low <= high)
+        {
+            mid = low + (high - low)/2;
+            if(arr[mid] == k)
+                return mid;
+            else if(arr[mid]<k)
+                low = mid +1;
+            else
+                high = mid -1;
+        }
+        return -1;
+    }
+};
