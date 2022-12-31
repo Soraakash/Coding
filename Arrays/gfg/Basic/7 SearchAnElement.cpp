02d4c0323379c2f569bbacd0c747c037d208bae7
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-01-01 05:07:31 +0530

    Day 1

---
+++ Arrays/gfg/Basic/7 SearchAnElement.cpp
@@ -0,0 +1,33 @@
+// Given an integer array and another integer element. The task is to find if the given element is present in array or not.
+
+// Example 1:
+
+// Input:
+// n = 4
+// arr[] = {1,2,3,4}
+// x = 3
+// Output: 2
+// Explanation: There is one test case 
+// with array as {1, 2, 3 4} and element 
+// to be searched as 3.  Since 3 is 
+// present at index 2, output is 2.
+
+// Expected Time Complexity: O(n).
+// Expected Auxiliary Space: O(1). 
+
+int search(int arr[], int N, int X)
+    {
+        
+        // Your code here
+        int ans = -1;
+        for(int i = 0;i<N;i++)
+        {
+            if(arr[i] == X)
+            {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+        
+    }
