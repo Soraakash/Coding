1d288115c52a8fc26b445588930991cf307705a7
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-01-01 08:24:57 +0530

    Day 1

---
+++ Arrays/gfg/Basic/14 StackUsingArray.cpp
@@ -0,0 +1,19 @@
+// Write a program to implement a Stack using Array. Your task is to use the class as shown in the comments in the code editor
+// and complete the functions push() and pop() to implement a stack.
+
+  
+  void MyStack :: push(int x)
+{
+    // Your Code
+    top++;
+    arr[top] = x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    // Your Code 
+    if(top == -1)
+        return -1;
+    return arr[top--];
+}
